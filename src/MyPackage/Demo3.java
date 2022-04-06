bc62ec0b79e00fee841b11f97ce4b71c29fad9b2
Shabana <shabana.sk.ind@gmail.com>
2022-04-06 11:35:58 +0530

    Revert "Added three Strings"
    This reverts commit 83784e05266abc9e2a244110fb6e011d0edf43ff.

--- src/MyPackage/Demo3.java
+++ src/MyPackage/Demo3.java
@@ -4,9 +4,6 @@ public class Demo3 {
 
 	public static void main(String[] args) {
 		System.out.println("This is my Demo3 Class");
-		String a="Hello";
-		String b="World";
-		String c="Java";
 
 	}
 
